e62c319d99e898dca327c5906c2670e1387ef040
DSHOBY <EMMANUELLE.CORREIA@OUTLOOK.FR>
2017-04-29 23:24:23 +0200

    update

--- ecom-maven/ecom-dao/src/main/java/com/gl/ecom/data/dao/database/Database.java
+++ ecom-maven/ecom-dao/src/main/java/com/gl/ecom/data/dao/database/Database.java
@@ -9,7 +9,7 @@ public class Database {
 
     public static Map<String, Catalogue> tableCatalogue;
     public static Map<String, Article> tableArticle;
-    public static Article article = new Article(AutoIncrement.autoId(Article.class),"pull",TypeEnum.PRETAPORTER,ColorEnum.BLANC,"XXL");
+   /* public static Article article = new Article(AutoIncrement.autoId(Article.class),"pull",TypeEnum.PRETAPORTER,ColorEnum.BLANC,"XXL");
     public static Article article2 = new Article(AutoIncrement.autoId(Article.class),"sac",TypeEnum.ACCESSOIRE,ColorEnum.NOIR,"Unique");
     public static Article article3 = new Article(AutoIncrement.autoId(Article.class),"montre",TypeEnum.LUXE,ColorEnum.BLANC,"Unique");
     public static Article article4 = new Article(AutoIncrement.autoId(Article.class),"montre",TypeEnum.LUXE,ColorEnum.BLANC,"Unique");
@@ -24,13 +24,13 @@ public class Database {
     public static Article article14 = new Article(AutoIncrement.autoId(Article.class),"montre",TypeEnum.LUXE,ColorEnum.BLANC,"Unique");
     public static Catalogue catalogue = new Catalogue("Homme");
     public static Catalogue catalogue2 = new Catalogue("Femme - Accessoires");
-    public static Catalogue catalogue3 = new Catalogue("Homme - Accessoires");
+    public static Catalogue catalogue3 = new Catalogue("Homme - Accessoires");*/
 
 
 static {
     tableArticle = new HashMap<String,Article>();
     tableCatalogue = new HashMap<String,Catalogue>();
-    tableArticle.put(article.getId(),article);
+   /* tableArticle.put(article.getId(),article);
     tableArticle.put(article2.getId(),article2);
     tableArticle.put(article3.getId(),article3);
     tableArticle.put(article4.getId(),article3);
@@ -45,7 +45,7 @@ static {
     tableArticle.put(article14.getId(),article3);
     tableCatalogue.put(catalogue.getId(),catalogue);
     tableCatalogue.put(catalogue2.getId(),catalogue2);
-    tableCatalogue.put(catalogue3.getId(),catalogue3);
+    tableCatalogue.put(catalogue3.getId(),catalogue3);*/
 
 }
 
